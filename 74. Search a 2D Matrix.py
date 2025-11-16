0cfa1ce2e239c8f15dbe7c73649ec8e159e00bc8
Nandini Bedola Srinivasulu <bsnandini000@gmail.com>
2025-11-16 15:14:15 -0600

    Implement binary search in 2D matrix

---
+++ 74. Search a 2D Matrix.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+
+        left, right = 0, m * n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            row = mid // n
+            col = mid % n
+            val = matrix[row][col]
+
+            if val == target:
+                return True
+            elif val < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
