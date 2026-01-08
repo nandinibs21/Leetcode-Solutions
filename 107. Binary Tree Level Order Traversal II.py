2a397fd72508c8b02fc47b0138a4bf833c10ed85
Nandini Bedola Srinivasulu <bsnandini000@gmail.com>
2026-01-07 22:59:29 -0600

    Add binary tree level order traversal II solution
    Implement level order traversal of a binary tree from bottom to top.

---
+++ 107. Binary Tree Level Order Traversal II.py
@@ -0,0 +1,34 @@
+from collections import deque
+from typing import List, Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val: int = 0, left: "TreeNode" = None, right: "TreeNode" = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        q = deque([root])
+        levels = []
+
+        while q:
+            level_size = len(q)
+            level = []
+
+            for _ in range(level_size):
+                node = q.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            levels.append(level)
+
+        return levels[::-1]
