a5bf01ea66047b4c70d5921ee1f0768b0574ddd3
Nandini Bedola Srinivasulu <bsnandini000@gmail.com>
2026-01-18 22:44:03 -0600

    Implement solution for populating next right pointers

---
+++ 117. Populating Next Right Pointers in Each Node II.py
@@ -0,0 +1,32 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+
+        curr = root  # current level's head
+        while curr:
+            dummy = Node(0)   # head for next level
+            tail = dummy      # tail to build next level's linked list
+
+            # traverse current level using next pointers
+            while curr:
+                if curr.left:
+                    tail.next = curr.left
+                    tail = tail.next
+                if curr.right:
+                    tail.next = curr.right
+                    tail = tail.next
+                curr = curr.next
+
+            # move to next level
+            curr = dummy.next
+
+        return root
