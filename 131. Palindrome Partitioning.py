57717eec4a1219da62b0b225f09f6c830cc1a17a
Nandini Bedola Srinivasulu <bsnandini000@gmail.com>
2026-02-03 20:23:02 -0600

    Implement palindrome partitioning algorithm

---
+++ 131. Palindrome Partitioning.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        n = len(s)
+        # pal[i][j] = True if s[i:j+1] is palindrome
+        pal = [[False] * n for _ in range(n)]
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                if s[i] == s[j] and (j - i <= 2 or pal[i + 1][j - 1]):
+                    pal[i][j] = True
+
+        res: List[List[str]] = []
+        path: List[str] = []
+
+        def dfs(start: int) -> None:
+            if start == n:
+                res.append(path.copy())
+                return
+            for end in range(start, n):
+                if pal[start][end]:
+                    path.append(s[start:end + 1])
+                    dfs(end + 1)
+                    path.pop()
+
+        dfs(0)
+        return res
